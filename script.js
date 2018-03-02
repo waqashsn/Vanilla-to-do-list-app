6579ba5e852bf5a84b1b6edd5681fd5811fcc4f7
WH <waqashsn@gmail.com>
2018-03-02 12:43:12 +0500

    Basic functionality achieved. Created event listeners and function to add new tasks to the list.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello world!!!!!");
+console.log("Script started...");
+
+//add eventlistener to "Add" button
+document.getElementById("taskAddBtn").addEventListener("click", addTask);
+
+var taskList = document.getElementById("taskList"); //assign the task list <ul> node to the taskList var
+var taskTextBox = document.getElementById("taskTextBox"); //assign the new task textbox to taskTextBox var
+
+//function that is invoked when add button is clicked
+function addTask(){
+      
+    var newTask = document.createElement("li"); //create a list item node
+    var newTaskText = document.createTextNode(taskTextBox.value); //create text node
+    newTask.appendChild(newTaskText); //add text node to list item
+    taskList.appendChild(newTask); //add list item to ul node
+    taskTextBox.value = ""; //Empty the textbox after adding task
+}
