7196fbf3c0266a930ce08a1c80b6a8984b4c5704
WH <waqashsn@gmail.com>
2018-03-02 13:08:46 +0500

    Changed the method of adding new task. Now passing the new task name as variable to addTask funtion, instead of reading it from textbox from inside the function. Acheived this functionality by calling an anonymouse function at click event of the Add button, which in turn calls the addTask function along with the passed argument.

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 console.log("Script started...");
 
-//add eventlistener to "Add" button
-document.getElementById("taskAddBtn").addEventListener("click", addTask);
-
 var taskList = document.getElementById("taskList"); //assign the task list <ul> node to the taskList var
 var taskTextBox = document.getElementById("taskTextBox"); //assign the new task textbox to taskTextBox var
 
-//function that is invoked when add button is clicked
-function addTask(){
-      
-    var newTask = document.createElement("li"); //create a list item node
-    var newTaskText = document.createTextNode(taskTextBox.value); //create text node
-    newTask.appendChild(newTaskText); //add text node to list item
-    taskList.appendChild(newTask); //add list item to ul node
-    taskTextBox.value = ""; //Empty the textbox after adding task
+//add eventlistener to "Add" button and pass the new task name by reading from textbox, inside an anonymouse function
+document.getElementById("taskAddBtn").addEventListener("click", function() {
+  addTask(taskTextBox.value);
+});
+
+//function that is invoked by anonymouse function when add button is clicked
+function addTask(taskName) {
+  var newTask = document.createElement("li"); //create a list item node
+  var newTaskText = document.createTextNode(taskName); //create text node
+  newTask.appendChild(newTaskText); //add text node to list item
+  taskList.appendChild(newTask); //add list item to ul node
+  taskTextBox.value = ""; //Empty the textbox after adding task
 }
