bfed034970f35d31b31dd655eb9b8d188b08cdcc
WH <waqashsn@gmail.com>
2018-03-05 13:12:15 +0500

    Added task counter. Functionality achienved by adding counter inside HTML span element in index.html, and creating a function that get the current count of task by counting the number of child elements of the task list <ul>. Task counter is printed on start, updated on new task addition and task removal

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 var taskList = document.getElementById("taskList"); //assign the task list <ul> node to the taskList var
 var taskTextBox = document.getElementById("taskTextBox"); //assign the new task textbox to taskTextBox var
 var listArea = document.getElementById("listArea");
+var taskCount = document.getElementById("counter"); //assign the task counter element to taskCount var
+taskCount.innerText = taskCounter(); //print the current number of tasks
 
 //add eventlistener to "Add" button and pass the new task name by reading from textbox, inside an anonymouse function
 //if statement checks if textbox is empty, if its empty, task is not added.
@@ -20,10 +22,19 @@ document.getElementById("taskTextBox").addEventListener("keyup",  function(event
     }
 })
 
+//function to see how many tasks are in the list
+function taskCounter() {
+    if(taskList.childElementCount != 0) {
+        return taskList.childElementCount; //return how many chilren <li>s the <ul> have
+    }
+    else return 0;
+}
+
 //function that removes the task by receiving the unique taskid
 function removeTask(taskId) {
-    document.getElementById(taskId).remove();
+    document.getElementById(taskId).remove(); //remove the element with passed taskId
     //console.log(itemId); 
+    taskCount.innerText = taskCounter(); //print the updated task counter after removal
 }
 
 //function that is invoked by anonymouse function when add button is clicked
@@ -50,5 +61,6 @@ function addTask(taskName) {
     newTaskLi.appendChild(newTaskCheckbox); //append task checkbox to the li
     newTaskLi.appendChild(label);//append label to the li
     taskList.appendChild(newTaskLi); //add list item to ul node
+    taskCount.innerText = taskCounter(); //update the task counter after addition
     taskTextBox.value = ""; //Empty the textbox after adding task
 }
